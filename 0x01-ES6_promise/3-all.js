0995b4118c63a77172a23cc2aa92213a3aad0a76
Aladaabdul <aladarahman18@gmail.com>
2024-04-24 21:20:32 +0100

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
-import { createUser } from './utils';
+import {uploadPhoto, createUser} from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([createUser()])
-    .then(([user]) => {
-      console.log(`${user.firstName} ${user.lastName}`);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
