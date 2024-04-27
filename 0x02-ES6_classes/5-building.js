b001e2fab8076c2445a59970c878c3d15983885c
Aladaabdul <aladarahman18@gmail.com>
2024-04-27 21:44:38 +0100

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+export default class Building {
+
+	constructor(sqft) {
+
+		if (typeof sqft !== 'number') {
+			throw new TypeError("sqft must be a Number");
+		}
+		this._sqft = sqft
+	}
+
+	get sqft() {
+		return this._sqft;
+	};
+
+	set sqft(value) {
+		if (typeof value !== 'number') {
+			throw new TypeError("sqft must be a Number");
+		}
+		this._sqft = value;
+	}
+
+	evacuationWarningMessage() {
+		throw new Error("Class extending Building must override evacuationWarningMessage");
+	}
+};
