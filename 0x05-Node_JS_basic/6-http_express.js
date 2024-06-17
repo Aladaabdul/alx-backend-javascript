ab5c54e5927decf3045b4d9a3f53b0543f58b2c3
aladaabdul <aladarahman18@gmail.com>
2024-06-17 09:09:11 +0100

    6-http_express.js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+// 6-http_express.js
+
+
+const express = require("express")
+const app = express()
+const PORT = 1245
+
+
+app.get('/', (req, res) => {
+	res.send("Hello Holberton School!")
+});
+
+app.listen(PORT, () => {
+	console.log("Server is running");
+});
