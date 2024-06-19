5a2d20469abb8ade237e03dddc5283a96622587f
aladaabdul <aladarahman18@gmail.com>
2024-06-19 21:47:56 +0100

    test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,19 +5,23 @@ const assert = require("assert");
 
 
 describe("sumNumber", function() {
-	it("sum of 1 and 3 should return 4" function() {
-		assert.equal(calculateNumber(1, 3), 4)
+	it("sum of 1 and 3 should return 4", function() {
+		assert.strictEqual(calculateNumber(1, 3), 4)
 	});
 
-	it("sum of 1 and 3.7 should return 5" function() {
-		assert.equal(calculateNumber(1, 3.7), 5)
+	it("sum of 1 and 3.7 should return 5", function() {
+		assert.strictEqual(calculateNumber(1, 3.7), 5)
 	});
 	
-	it("sum of 1.2 and 3.7 should return 5" function() {
-		assert.equal(calculateNumber(1.2, 3.7), 5)
+	it("sum of 1.2 and 3.7 should return 5", function() {
+		assert.strictEqual(calculateNumber(1.2, 3.7), 5)
 	});
 
-	it("sum of 1.5 and 3.7 should return 6" function() {
-		assert.equal(calculateNumber(1.5, 3.7), 6)
+	it("sum of 1.5 and 3.7 should return 6", function() {
+		assert.strictEqual(calculateNumber(1.5, 3.7), 6)
+	});
+
+	it("sum of -0.4 and 0.4", function() {
+		assert.strictEqual(calculateNumber(-0.4, 0.4), 0)
 	});
 });
