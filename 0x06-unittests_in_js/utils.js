c0d340ed44e6ea7110405ffa4c1d7045845a6c0f
aladaabdul <aladarahman18@gmail.com>
2024-06-21 06:05:29 +0100

    test

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,27 @@
+// utils.js
+
+class Utils {
+	
+	static calculateNumber(type, a, b) {
+
+        const num1 = Math.round(a);
+        const num2 = Math.round(b);
+
+        if (type == "SUM") {
+                return num1 + num2;
+        }
+        else if (type == "SUBTRACT") {
+                return num1 - num2;
+        }
+        else {
+                if (num2 == 0) {
+                        return "Error";
+                }
+                return num1 / num2
+	}
+
+	}
+
+}
+
+module.exports = Utils;
