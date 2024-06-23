362b8f5c130865adcfce276c84468e47a62c4804
aladaabdul <aladarahman18@gmail.com>
2024-06-23 23:16:58 +0100

    api testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -4,25 +4,27 @@
 const chai = require("chai");
 const expect = chai.expect
 const request = require("request");
-
-let app;
+const app = require("./api.js");
 
 describe('API integration test', () => {
+  let server
 
   const API_URL = 'http://localhost:7865';
 
   beforeEach(function() {
-	  app = require("./api.js");
+	  server = app.listen(7865, done);
   });
   
   afterEach(function() {
-	  app.close
+	  server.close(done);
   });
 
-  it('GET / returns correct response', () => {
+  it('GET / returns correct response', (done) => {
 	  request.get(`${API_URL}/`, function (error, response, body) {
+		  if (error) return done(error);
 		  expect(response.statusCode).to.be.equal(200);
 		  expect(body).to.be.equal('Welcome to the payment system');
+		  done();
 	  });
   });
 });
